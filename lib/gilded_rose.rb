62963becd05b8bb581da936bfdbe3bc9d23dfb7d
Julian Churchill <chewielouie03@gmail.com>
2012-09-30 21:37:19 +0100

    Removed pointless brackets

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -27,36 +27,36 @@ class GildedRose
 
     @items.each_with_index do |item, i|
       if is_normal? item
-        if (item.quality > MIN_QUALITY)
-          if (item.name != "Sulfuras, Hand of Ragnaros")
+        if item.quality > MIN_QUALITY
+          if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality -= 1
           end
         end
       else
-        if (item.quality < MAX_QUALITY)
+        if item.quality < MAX_QUALITY
           item.quality += 1
-          if (item.name == "Backstage passes to a TAFKAL80ETC concert")
-            if (item.sell_in < 11)
-              if (item.quality < MAX_QUALITY)
+          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+            if item.sell_in < 11
+              if item.quality < MAX_QUALITY
                 item.quality += 1
               end
             end
-            if (item.sell_in < 6)
-              if (item.quality < MAX_QUALITY)
+            if item.sell_in < 6
+              if item.quality < MAX_QUALITY
                 item.quality += 1
               end
             end
           end
         end
       end
-      if (item.name != "Sulfuras, Hand of Ragnaros")
+      if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in -= 1
       end
-      if (item.sell_in < 0)
-        if (item.name != "Aged Brie")
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert")
-            if (item.quality > MIN_QUALITY)
-              if (item.name != "Sulfuras, Hand of Ragnaros")
+      if item.sell_in < 0
+        if item.name != "Aged Brie"
+          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+            if item.quality > MIN_QUALITY
+              if item.name != "Sulfuras, Hand of Ragnaros"
                 item.quality -= 1
               end
             end
@@ -64,7 +64,7 @@ class GildedRose
             item.quality = 0
           end
         else
-          if (item.quality < MAX_QUALITY)
+          if item.quality < MAX_QUALITY
             item.quality += 1
           end
         end
