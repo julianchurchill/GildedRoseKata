04a7713c4f6140940d886b4ea415d813aea0f434
Julian Churchill <chewielouie03@gmail.com>
2012-09-30 22:42:11 +0100

    Added ShopItem class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,12 +1,71 @@
 require './lib/item.rb'
 
-class GildedRose
-
+class ShopItem
   MIN_QUALITY = 0
   MAX_QUALITY = 50
   BRIE = "Aged Brie"
   SULFURAS = "Sulfuras, Hand of Ragnaros"
   BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+
+  def initialize item
+    @item = item
+  end
+
+  def increase_age
+    @item.sell_in -= 1 if can_age?
+  end
+
+  def can_age?
+      @item.name != SULFURAS
+  end
+
+  def quality_reduces_over_time?
+      @item.name != BRIE && @item.name != BACKSTAGE_PASS
+  end
+
+  def can_degrade?
+      @item.name != SULFURAS
+  end
+
+  def reduce_quality
+    if @item.quality > MIN_QUALITY
+      @item.quality -= 1 if can_degrade?
+    end
+  end
+
+  def increase_quality
+    @item.quality += 1 if @item.quality < MAX_QUALITY
+  end
+
+  def add_approaching_expiry_quality_bonus
+     if @item.name == BACKSTAGE_PASS
+      increase_quality if @item.sell_in < 11
+      increase_quality if @item.sell_in < 6
+    end
+  end
+
+  def adjust_quality_of_expired_item
+    reduce_quality if quality_reduces_over_time?
+    @item.quality = 0 if @item.name == BACKSTAGE_PASS
+    increase_quality if @item.name == BRIE
+  end
+
+  def expired?
+    @item.sell_in < 0
+  end
+
+  def update_quality
+    if quality_reduces_over_time?
+      reduce_quality
+    else
+      increase_quality
+      add_approaching_expiry_quality_bonus
+    end
+    adjust_quality_of_expired_item if expired?
+  end
+end
+
+class GildedRose
 
   attr_reader :items
 
@@ -22,63 +81,11 @@ class GildedRose
     @items << Item.new("Conjured Mana Cake", 3, 6)
   end
 
-  def quality_reduces_over_time? item
-      item.name != BRIE && item.name != BACKSTAGE_PASS
-  end
-
-  def can_age? item
-      item.name != SULFURAS
-  end
-
-  def can_degrade? item
-      item.name != SULFURAS
-  end
-
-  def reduce_quality item
-    if item.quality > MIN_QUALITY
-      if can_degrade? item
-        item.quality -= 1
-      end
-    end
-  end
-
-  def increase_quality item
-    if item.quality < MAX_QUALITY
-      item.quality += 1
-    end
-  end
-
-  def expired? item
-    item.sell_in < 0
-  end
-
-  def age_item item
-    item.sell_in -= 1 if can_age? item
-  end
-
-  def adjust_quality_of_expired_item item
-    reduce_quality item if quality_reduces_over_time? item
-    item.quality = 0 if item.name == BACKSTAGE_PASS
-    increase_quality item if item.name == BRIE
-  end
-
-  def add_approaching_expiry_quality_bonus item
-     if item.name == BACKSTAGE_PASS
-      increase_quality item if item.sell_in < 11
-      increase_quality item if item.sell_in < 6
-    end
-  end
-
   def update_quality
-    @items.each do |item|
-      age_item item
-      if quality_reduces_over_time? item
-        reduce_quality item
-      else
-        increase_quality item
-        add_approaching_expiry_quality_bonus item
-      end
-      adjust_quality_of_expired_item item if expired? item
+    @items.each do |i|
+      item = ShopItem.new i
+      item.increase_age
+      item.update_quality
     end
   end
 
