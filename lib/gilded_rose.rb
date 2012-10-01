41386b077e7c1f679e8a59ddccc52558577f133e
Julian Churchill <chewielouie03@gmail.com>
2012-10-01 21:12:50 +0100

    Shortened increment/decrement statements

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -22,35 +22,35 @@ class GildedRose
       if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert")
         if (item.quality > 0)
           if (item.name != "Sulfuras, Hand of Ragnaros")
-            item.quality = item.quality - 1
+            item.quality -= 1
           end
         end
       else
         if (item.quality < 50)
-          item.quality = item.quality + 1
+          item.quality += 1
           if (item.name == "Backstage passes to a TAFKAL80ETC concert")
             if (item.sell_in < 11)
               if (item.quality < 50)
-                item.quality = item.quality + 1
+                item.quality += 1
               end
             end
             if (item.sell_in < 6)
               if (item.quality < 50)
-                item.quality = item.quality + 1
+                item.quality += 1
               end
             end
           end
         end
       end
       if (item.name != "Sulfuras, Hand of Ragnaros")
-        item.sell_in = item.sell_in - 1;
+        item.sell_in -= 1;
       end
       if (item.sell_in < 0)
         if (item.name != "Aged Brie")
           if (item.name != "Backstage passes to a TAFKAL80ETC concert")
             if (item.quality > 0)
               if (item.name != "Sulfuras, Hand of Ragnaros")
-                item.quality = item.quality - 1
+                item.quality -= 1
               end
             end
           else
@@ -58,7 +58,7 @@ class GildedRose
           end
         else
           if (item.quality < 50)
-            item.quality = item.quality + 1
+            item.quality += 1
           end
         end
       end
