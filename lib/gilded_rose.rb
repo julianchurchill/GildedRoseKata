9040ea51646c9df5738edd7c1515f108e5a51ea2
Julian Churchill <chewielouie03@gmail.com>
2012-10-01 21:15:10 +0100

    Removed gratuitous brackets

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -17,39 +17,38 @@ class GildedRose
   end
 
   def update_quality
-
     @items.each do |item|
-      if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert")
-        if (item.quality > 0)
-          if (item.name != "Sulfuras, Hand of Ragnaros")
+      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.quality > 0
+          if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality -= 1
           end
         end
       else
-        if (item.quality < 50)
+        if item.quality < 50
           item.quality += 1
-          if (item.name == "Backstage passes to a TAFKAL80ETC concert")
-            if (item.sell_in < 11)
-              if (item.quality < 50)
+          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+            if item.sell_in < 11
+              if item.quality < 50
                 item.quality += 1
               end
             end
-            if (item.sell_in < 6)
-              if (item.quality < 50)
+            if item.sell_in < 6
+              if item.quality < 50
                 item.quality += 1
               end
             end
           end
         end
       end
-      if (item.name != "Sulfuras, Hand of Ragnaros")
+      if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in -= 1;
       end
-      if (item.sell_in < 0)
-        if (item.name != "Aged Brie")
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert")
-            if (item.quality > 0)
-              if (item.name != "Sulfuras, Hand of Ragnaros")
+      if item.sell_in < 0
+        if item.name != "Aged Brie"
+          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+            if item.quality > 0
+              if item.name != "Sulfuras, Hand of Ragnaros"
                 item.quality -= 1
               end
             end
@@ -57,7 +56,7 @@ class GildedRose
             item.quality = item.quality - item.quality
           end
         else
-          if (item.quality < 50)
+          if item.quality < 50
             item.quality += 1
           end
         end
