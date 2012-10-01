f8cd0b41aea5822226459b34b7de3b92835e12fb
Julian Churchill <chewielouie03@gmail.com>
2012-10-01 21:11:27 +0100

    Replaced for loop with enumerator

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -18,47 +18,47 @@ class GildedRose
 
   def update_quality
 
-    for i in 0..(@items.size-1)
-      if (@items[i].name != "Aged Brie" && @items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-        if (@items[i].quality > 0)
-          if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-            @items[i].quality = @items[i].quality - 1
+    @items.each do |item|
+      if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert")
+        if (item.quality > 0)
+          if (item.name != "Sulfuras, Hand of Ragnaros")
+            item.quality = item.quality - 1
           end
         end
       else
-        if (@items[i].quality < 50)
-          @items[i].quality = @items[i].quality + 1
-          if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-            if (@items[i].sell_in < 11)
-              if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
+        if (item.quality < 50)
+          item.quality = item.quality + 1
+          if (item.name == "Backstage passes to a TAFKAL80ETC concert")
+            if (item.sell_in < 11)
+              if (item.quality < 50)
+                item.quality = item.quality + 1
               end
             end
-            if (@items[i].sell_in < 6)
-              if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
+            if (item.sell_in < 6)
+              if (item.quality < 50)
+                item.quality = item.quality + 1
               end
             end
           end
         end
       end
-      if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-        @items[i].sell_in = @items[i].sell_in - 1;
+      if (item.name != "Sulfuras, Hand of Ragnaros")
+        item.sell_in = item.sell_in - 1;
       end
-      if (@items[i].sell_in < 0)
-        if (@items[i].name != "Aged Brie")
-          if (@items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-            if (@items[i].quality > 0)
-              if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-                @items[i].quality = @items[i].quality - 1
+      if (item.sell_in < 0)
+        if (item.name != "Aged Brie")
+          if (item.name != "Backstage passes to a TAFKAL80ETC concert")
+            if (item.quality > 0)
+              if (item.name != "Sulfuras, Hand of Ragnaros")
+                item.quality = item.quality - 1
               end
             end
           else
-            @items[i].quality = @items[i].quality - @items[i].quality
+            item.quality = item.quality - item.quality
           end
         else
-          if (@items[i].quality < 50)
-            @items[i].quality = @items[i].quality + 1
+          if (item.quality < 50)
+            item.quality = item.quality + 1
           end
         end
       end
